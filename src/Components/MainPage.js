c8c0a28f6311e71e5e50cfab0c6d2b84fc49ae51
ranagoharayoub <ranagohar1163@gmail.com>
2021-05-01 01:16:24 +0500

    Change

--- src/Components/MainPage.js
+++ src/Components/MainPage.js
@@ -3,8 +3,18 @@ import './MainPage.css'
 
 function MainPage() {
     return (
-        <div>
-            MainPage
+        <div className='main-cont'>
+            <div className='main-upper-cont'>
+                <div className='navbar'>
+
+                </div>
+                <div className='main-content'>
+
+                </div>
+            </div>
+            <div className='main-lower-cont'>
+
+            </div>
         </div>
     )
 }
