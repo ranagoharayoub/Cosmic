65d4a469a4af0e17a581d93caf3e9f6ef3cba79f
ranagoharayoub <ranagohar1163@gmail.com>
2021-05-01 17:10:54 +0500

    Page Added

--- src/Components/FreedomForYou.jsx
+++ src/Components/FreedomForYou.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 import './FreedomForYou.css'
 function FreedomForYou() {
     return (
-        <div>
-            FreedomForYou
+        <div className='freedom'>
+            <div className='green-card'></div>
+            <div className='text'>“I need to be able to communicate the value proposition of the company in a simple format, where the customers can understand value of the different services offered within the company. I need a clean interface and a flow of communication where the value can be perceived instantly”</div>
+            <div className='carlos'>Carlos Busineesman</div>
+            <div className='img'></div>
         </div>
     )
 }
